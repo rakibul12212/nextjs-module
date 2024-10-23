f13df939e6afe3a5be3e525a14e1ca59b2c97ace
rakib12212 <rakibulislamr83@gmail.com>
2024-10-23 21:07:26 +0600

    nasted routing

---
+++ src/app/about/mission/page.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Mission = () => {
+    return (
+        <div>
+            <Link href="/about/mission/vision">vision</Link>
+        </div>
+    );
+};
+
+export default Mission;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 const AboutPage = () => {
-  return <div>about page</div>;
+  return (
+    <div className="text-center">
+      <h1>about page</h1>
+      <div className="flex justify-center space-x-8">
+        <Link href="/about/history">History</Link>
+        <Link href="/about/mission">Mission</Link>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
